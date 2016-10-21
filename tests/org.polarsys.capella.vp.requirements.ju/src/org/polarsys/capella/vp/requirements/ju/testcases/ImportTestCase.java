9d70c17b758caf28bd66bf66104f476e3b71f43e
Joao Barata <jean.barata@thalesgroup.com>
2016-10-21 17:16:53 +0200

    add import test case
    Change-Id: I76603a97cd268bb51d53b360c83979cd66e14c28
    Signed-off-by: Joao Barata <jean.barata@thalesgroup.com>

---
+++ tests/org.polarsys.capella.vp.requirements.ju/src/org/polarsys/capella/vp/requirements/ju/testcases/ImportTestCase.java
@@ -0,0 +1,70 @@
+/*******************************************************************************
+ * Copyright (c) 2016 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.vp.requirements.ju.testcases;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.sirius.business.api.session.Session;
+import org.polarsys.capella.core.data.cs.BlockArchitecture;
+import org.polarsys.capella.shared.id.handler.IScope;
+import org.polarsys.capella.shared.id.handler.IdManager;
+import org.polarsys.capella.test.framework.api.BasicTestCase;
+import org.polarsys.capella.test.framework.helpers.IResourceHelpers;
+import org.polarsys.capella.test.framework.helpers.TestHelper;
+import org.polarsys.capella.vp.requirements.importer.transposer.launcher.RequirementsImportLauncher;
+
+/**
+ * @author Joao Barata
+ */
+public class ImportTestCase extends BasicTestCase {
+
+  private static final String systemAnalysis = "24658239-7734-4c39-9402-83325c52d04c";
+  
+  private static final String inputFileName = "model/inputs/model1.reqif";
+
+  private static final String projectTestName = "emptymodel";
+
+  /**
+   * @see org.polarsys.capella.test.framework.api.BasicTestArtefact#getRequiredTestModels()
+   */
+  @Override
+  public List<String> getRequiredTestModels() {
+    return Arrays.asList(projectTestName);
+  }
+
+  /**
+   * @see org.polarsys.capella.test.framework.api.BasicTestCase#test()
+   */
+  @Override
+  public void test() throws Exception {
+    EObject target = IdManager.getInstance().getEObject(systemAnalysis, new IScope() {
+      @Override
+      public List<Resource> getResources() {
+        Session session = getSession(projectTestName);
+        Resource semanticResource = TestHelper.getSemanticResource(session);
+        return Collections.singletonList(semanticResource);
+      }
+    });
+    if (target instanceof BlockArchitecture) {
+      File file = IResourceHelpers.getFileOrFolderInTestPlugin(getClass(), inputFileName);
+      URI model = URI.createFileURI(file.getPath());
+
+      new RequirementsImportLauncher().launch(model, (BlockArchitecture) target, new NullProgressMonitor());
+    }
+  }
+}
