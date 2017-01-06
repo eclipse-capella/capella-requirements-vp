ae089a621bf510f637d7c90b311080a023d2d731
Joao Barata <jean.barata@thalesgroup.com>
2017-01-06 16:51:45 +0100

    [1380] Several issues on properties view
    Bug: 1380
    Change-Id: I0908bdd96b00847b9c2a73a4a5cf08dc1bc379a8
    Signed-off-by: Joao Barata <jean.barata@thalesgroup.com>

---
+++ plugins/org.polarsys.capella.vp.requirements.ui.properties/src/org/polarsys/capella/vp/requirements/ui/properties/sections/CapellaOutgoingRelationPropertySection.java
@@ -0,0 +1,108 @@
+/*******************************************************************************
+ * Copyright (c) 2017 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.vp.requirements.ui.properties.sections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
+import org.polarsys.capella.core.ui.properties.controllers.SimpleSemanticFieldController;
+import org.polarsys.capella.core.ui.properties.fields.AbstractSemanticField;
+import org.polarsys.capella.core.ui.properties.fields.SimpleSemanticField;
+import org.polarsys.capella.core.ui.properties.sections.AbstractSection;
+import org.polarsys.capella.vp.requirements.CapellaRequirements.CapellaOutgoingRelation;
+import org.polarsys.capella.vp.requirements.CapellaRequirements.CapellaRequirementsPackage;
+import org.polarsys.capella.vp.requirements.ui.properties.Messages;
+import org.polarsys.kitalpha.vp.requirements.ui.properties.KitalphaRequirementsUIPropertiesPlugin;
+import org.polarsys.kitalpha.vp.requirements.ui.properties.fields.BasicReqIFElementGroup;
+
+/**
+ * @author Joao Barata
+ */
+public class CapellaOutgoingRelationPropertySection extends AbstractSection {
+
+  protected SimpleSemanticField sourceField;
+  protected SimpleSemanticField targetField;
+  protected BasicReqIFElementGroup reqIFElementGroup;
+
+	/**
+	 * @param eObject current object
+	 */
+	public boolean select(Object eObject) {
+		EObject eObjectToTest = super.selection(eObject);
+
+		if (KitalphaRequirementsUIPropertiesPlugin.isViewpointActive(eObjectToTest) && eObjectToTest instanceof CapellaOutgoingRelation) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	* @param part
+	* @param selection
+	*/
+	public void setInput(IWorkbenchPart part, ISelection selection) {
+		EObject newEObject = super.setInputSelection(part, selection);
+
+		if (newEObject instanceof CapellaOutgoingRelation) {
+			loadData(newEObject);
+		}
+	}
+
+	/**
+	 * @param parent
+	 * @param aTabbedPropertySheetPage
+	 */
+	public void createControls(Composite parent, TabbedPropertySheetPage aTabbedPropertySheetPage) {
+		super.createControls(parent, aTabbedPropertySheetPage);
+
+    boolean displayedInWizard = isDisplayedInWizard();
+
+    reqIFElementGroup = new BasicReqIFElementGroup(_rootParentComposite, getWidgetFactory(), true);
+    reqIFElementGroup.setDisplayedInWizard(displayedInWizard);
+
+    sourceField = new SimpleSemanticField(getReferencesGroup(),
+      Messages.getString("CapellaOutgoingRelation.SourceLabel"), getWidgetFactory(), new SimpleSemanticFieldController()); //$NON-NLS-1$
+    sourceField.setDisplayedInWizard(displayedInWizard);
+
+    targetField = new SimpleSemanticField(getReferencesGroup(),
+      Messages.getString("CapellaOutgoingRelation.TargetLabel"), getWidgetFactory(), new SimpleSemanticFieldController()); //$NON-NLS-1$
+    targetField.setDisplayedInWizard(displayedInWizard);
+	}
+
+	/**
+	 * @param capellaElement
+	 */
+	public void loadData(EObject capellaElement) {
+		super.loadData(capellaElement);
+
+    reqIFElementGroup.loadData(capellaElement);
+    sourceField.loadData(capellaElement, CapellaRequirementsPackage.eINSTANCE.getCapellaOutgoingRelation_Source());
+    targetField.loadData(capellaElement, CapellaRequirementsPackage.eINSTANCE.getCapellaOutgoingRelation_Target());
+  }
+
+	/**
+	 * 
+   */
+	public List<AbstractSemanticField> getSemanticFields() {
+		List<AbstractSemanticField> abstractSemanticFields = new ArrayList<AbstractSemanticField>();
+
+		abstractSemanticFields.add(reqIFElementGroup);
+		abstractSemanticFields.add(sourceField);
+    abstractSemanticFields.add(targetField);
+
+		return abstractSemanticFields;
+	}
+}
