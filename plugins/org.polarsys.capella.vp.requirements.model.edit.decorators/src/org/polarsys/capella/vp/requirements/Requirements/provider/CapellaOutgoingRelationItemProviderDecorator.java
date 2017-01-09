fe5cb006a9910e4173969eeb312f693a232ed7c9
Tu Ton <minhtutonthat@gmail.com>
2017-01-09 16:23:10 +0100

    [1384] [Requirements VP] Add filters for technical elements and change label for incoming/outgoing relations
    Bug: 1384
    Change-Id: I713467267a15b4c3d2ee41722f806e459322a109
    Signed-off-by: Tu Ton <minhtutonthat@gmail.com>

--- plugins/org.polarsys.capella.vp.requirements.model.edit.decorators/src/org/polarsys/capella/vp/requirements/Requirements/provider/CapellaOutgoingRelationItemProviderDecorator.java
+++ plugins/org.polarsys.capella.vp.requirements.model.edit.decorators/src/org/polarsys/capella/vp/requirements/Requirements/provider/CapellaOutgoingRelationItemProviderDecorator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 THALES GLOBAL SERVICES.
+ * Copyright (c) 2016, 2017 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -39,9 +39,9 @@ public class CapellaOutgoingRelationItemProviderDecorator extends
 
     RelationType type = relation.getRelationType();
     if (type != null) {
-      return "[" + type.getReqIFLongName() + "] " + EObjectLabelProviderHelper.getText(relation.getTarget());
+      return "[" + type.getReqIFLongName() + "] to " + EObjectLabelProviderHelper.getText(relation.getTarget());
     }
 
-    return super.getText(object);
+    return "[Capella Outgoing Relation] to " + EObjectLabelProviderHelper.getText(relation.getTarget());
   }
 }
