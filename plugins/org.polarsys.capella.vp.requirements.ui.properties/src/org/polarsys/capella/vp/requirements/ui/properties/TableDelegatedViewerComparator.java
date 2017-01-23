11b672ddd1079e073b99df9676e39f95865db045
Philippe DUL <philippe.dul@thalesgroup.com>
2017-01-23 11:02:00 +0100

    [1413][Requirement VP] Add a sorter to the Allocation wizard
    Bug: 1413
    Change-Id: I13300901819c2ce201d200711e5eae889700db2c
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

--- plugins/org.polarsys.capella.vp.requirements.ui.properties/src/org/polarsys/capella/vp/requirements/ui/properties/TableDelegatedViewerComparator.java
+++ plugins/org.polarsys.capella.vp.requirements.ui.properties/src/org/polarsys/capella/vp/requirements/ui/properties/TableDelegatedViewerComparator.java
@@ -10,101 +10,55 @@
  *******************************************************************************/
 package org.polarsys.capella.vp.requirements.ui.properties;
 
-import org.eclipse.core.resources.IMarker;
-import org.eclipse.emf.edit.provider.IItemLabelProvider;
+import org.eclipse.jface.viewers.CellLabelProvider;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerComparator;
 import org.eclipse.swt.SWT;
-import org.polarsys.capella.common.mdsofa.common.constant.ICommonConstants;
-import org.polarsys.capella.core.data.capellacore.CapellaElement;
-import org.polarsys.kitalpha.vp.requirements.Requirements.AbstractRelation;
-import org.polarsys.kitalpha.vp.requirements.Requirements.Requirement;
-import org.polarsys.kitalpha.vp.requirements.Requirements.provider.RequirementsItemProviderDecoratorAdapterFactory;
-
 
 public class TableDelegatedViewerComparator extends ViewerComparator {
-  private int propertyIndex;
-  private static final int DESCENDING = 1;
-  private int direction = DESCENDING;
+  
+  private int columnIndex;
+  
+  private boolean descending;
 
   public TableDelegatedViewerComparator() {
-          this.propertyIndex = 0;
-          direction = DESCENDING;
+    this.columnIndex = 0;
+    descending = true;
   }
 
   public int getDirection() {
-          return direction == 1 ? SWT.DOWN : SWT.UP;
+    return descending ? SWT.DOWN : SWT.UP;
   }
 
   public void setColumn(int column) {
-          if (column == this.propertyIndex) {
-                  // Same column as last sort; toggle the direction
-                  direction = 1 - direction;
-          } else {
-                  // New column; do an ascending sort
-                  this.propertyIndex = column;
-                  direction = DESCENDING;
-          }
-  }
-  
-  @Override
-  /**
-   * This makes sure that nodes with children are always on top
-   * {@inheritDoc}
-   */
-  public int category(Object o) {
-    if (o instanceof IMarker) {
-      return 100;
+    if (column == this.columnIndex) {
+      // Same column as last sort; toggle the direction
+      descending = !descending;
+    } else {
+      // New column; do an ascending sort
+      this.columnIndex = column;
+      descending = true;
     }
-    return 0;
   }
 
   @Override
   public int compare(Viewer viewer, Object o1, Object o2) {
-      int rc = 0;
-      
-      int cat1 = category(o1);
-      int cat2 = category(o2);
-      if (cat1 != cat2) {
-        rc = cat1 - cat2;
-      }
-      
-      // requirements comparison
-      if ((o1 instanceof Requirement) && (o2 instanceof Requirement)){
-           // extract req o1's label
-           IItemLabelProvider adapted1 = (IItemLabelProvider) (new RequirementsItemProviderDecoratorAdapterFactory())
-                    .adapt(o1, IItemLabelProvider.class);
-           String s1 = adapted1.getText(o1);
-           
-           // extract req o2's label
-           IItemLabelProvider adapted2 = (IItemLabelProvider) (new RequirementsItemProviderDecoratorAdapterFactory())
-                    .adapt(o2, IItemLabelProvider.class);
-           String s2 = adapted2.getText(o2);
-           
-           if (s1 == null) {
-               s1 = ICommonConstants.EMPTY_STRING;
-           }
-           if (s2 == null) {
-               s2 = ICommonConstants.EMPTY_STRING;
-           }
-           
-           rc = s1.compareTo(s2);
-      }
-     
-      // capella elements comparison
-      if ((o1 instanceof CapellaElement) && (o2 instanceof CapellaElement)) {
-          rc = o1.toString().compareTo(o2.toString());
-      }
-     
-      // relation types comparison
-      if ((o1 instanceof AbstractRelation) && (o2 instanceof AbstractRelation)){
-          rc = o1.toString().compareTo(o2.toString());
-      }
-      
-      //if descending order, flip the direction
-      if(direction == DESCENDING) {
-        rc = -rc;
-      }
-      return rc;
+    int rc = 0;
+
+    CellLabelProvider provider = ((TableViewer) viewer).getLabelProvider(columnIndex);
+    if (provider instanceof ILabelProvider) {
+      ILabelProvider labelProvider = (ILabelProvider) provider;
+      String s1 = labelProvider.getText(o1);
+      String s2 = labelProvider.getText(o2);
+      rc = s2.compareTo(s1);
+    }
+
+    // if descending order, flip the direction
+    if (descending) {
+      rc = -rc;
+    }
+    return rc;
   }
 }
