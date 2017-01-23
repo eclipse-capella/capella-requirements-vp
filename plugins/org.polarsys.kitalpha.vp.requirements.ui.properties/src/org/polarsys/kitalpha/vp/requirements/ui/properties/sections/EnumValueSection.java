13f0ff7a18c8ea7a4f9fc892df2b404c98ab8e24
Ali AKAR <ali.akar82@gmail.com>
2017-01-23 11:01:13 +0100

    [1415] Missing Requirement VP section for EnumValue
    Bug 1415
    
    Change-Id: I52ec2d335ab524ff01777e14d205f340e699d797
    Signed-off-by: Ali AKAR <ali.akar82@gmail.com>

---
+++ plugins/org.polarsys.kitalpha.vp.requirements.ui.properties/src/org/polarsys/kitalpha/vp/requirements/ui/properties/sections/EnumValueSection.java
@@ -0,0 +1,87 @@
+/*******************************************************************************
+ * Copyright (c) 2016, 2017 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.kitalpha.vp.requirements.ui.properties.sections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
+import org.polarsys.capella.core.ui.properties.fields.AbstractSemanticField;
+import org.polarsys.capella.core.ui.properties.sections.AbstractSection;
+import org.polarsys.kitalpha.vp.requirements.Requirements.EnumValue;
+import org.polarsys.kitalpha.vp.requirements.ui.properties.KitalphaRequirementsUIPropertiesPlugin;
+import org.polarsys.kitalpha.vp.requirements.ui.properties.fields.BasicReqIFElementGroup;
+
+public class EnumValueSection extends AbstractSection {
+
+  protected BasicReqIFElementGroup reqIFElementGroup;
+
+	/**
+	 * @param eObject current object
+	 */
+	public boolean select(Object eObject) {
+		EObject eObjectToTest = super.selection(eObject);
+
+		if (KitalphaRequirementsUIPropertiesPlugin.isViewpointActive(eObjectToTest) && eObjectToTest instanceof EnumValue) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	* @param part
+	* @param selection
+	*/
+	public void setInput(IWorkbenchPart part, ISelection selection) {
+		EObject newEObject = super.setInputSelection(part, selection);
+
+		if (newEObject instanceof EnumValue) {
+			loadData(newEObject);
+		}
+	}
+
+	/**
+	 * @param parent
+	 * @param aTabbedPropertySheetPage
+	 */
+	public void createControls(Composite parent, TabbedPropertySheetPage aTabbedPropertySheetPage) {
+		super.createControls(parent, aTabbedPropertySheetPage);
+
+    boolean displayedInWizard = isDisplayedInWizard();
+
+    reqIFElementGroup = new BasicReqIFElementGroup(_rootParentComposite, getWidgetFactory(), true);
+    reqIFElementGroup.setDisplayedInWizard(displayedInWizard);
+	}
+
+	/**
+	 * @param capellaElement
+	 */
+	public void loadData(EObject capellaElement) {
+		super.loadData(capellaElement);
+
+    reqIFElementGroup.loadData(capellaElement);
+  }
+
+	/**
+	 * 
+   */
+	public List<AbstractSemanticField> getSemanticFields() {
+		List<AbstractSemanticField> abstractSemanticFields = new ArrayList<AbstractSemanticField>();
+
+		abstractSemanticFields.add(reqIFElementGroup);
+
+		return abstractSemanticFields;
+	}
+}
