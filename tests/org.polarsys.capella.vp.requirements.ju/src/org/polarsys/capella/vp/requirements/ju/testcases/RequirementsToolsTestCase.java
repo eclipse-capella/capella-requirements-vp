949ce471cc663f1f31b1b9cd627d94746a5f331a
cong-bang.do <docongbang1993@gmail.com>
2018-07-06 16:57:48 +0200

    [2105] Ability to show allocated Requirements in Diagrams
    Add Junit, no test on diagram yet
    
    Bug: 2105
    Change-Id: I76afd06c1dac4d8e09149a560a0298fe3e2d5ad9
    Signed-off-by: Jeremy Aubry <jaubry@obeo.fr>
    Signed-off-by: cong-bang.do <docongbang1993@gmail.com>

--- tests/org.polarsys.capella.vp.requirements.ju/src/org/polarsys/capella/vp/requirements/ju/testcases/RequirementsToolsTestCase.java
+++ tests/org.polarsys.capella.vp.requirements.ju/src/org/polarsys/capella/vp/requirements/ju/testcases/RequirementsToolsTestCase.java
@@ -10,6 +10,9 @@
  *******************************************************************************/
 package org.polarsys.capella.vp.requirements.ju.testcases;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.sirius.business.api.dialect.DialectManager;
 import org.eclipse.sirius.business.api.session.Session;
@@ -23,13 +26,13 @@ import org.polarsys.capella.test.framework.context.SessionContext;
 import org.polarsys.capella.vp.requirements.ju.misc.RequirementsDiagram;
 
 public class RequirementsToolsTestCase extends AbstractDiagramTestCase {
-  
+
   private static String REQ_1 = "fe5ca911-1f2f-40eb-932b-3e4dab3eed86";
   private static String REQ_2 = "269f08ec-513d-41fd-96ec-1ee6c2dbd8cd";
   private static String REQ_3 = "bd488918-1fa9-4278-a9c9-18874d4ddeb2";
   private static String REQ_4 = "4eff0b4f-cbf5-434b-b021-69b882b6e2f5";
   private static String REQ_5 = "aae247d0-5cc1-4dba-8a26-a9370c976366";
-  
+
   @Override
   protected String getRequiredTestModel() {
     return "In-Flight Entertainment System With Requirements";
@@ -39,17 +42,23 @@ public class RequirementsToolsTestCase extends AbstractDiagramTestCase {
   public void test() throws Exception {
     Session session = getSession(getRequiredTestModel());
     SessionContext sessionContext = new SessionContext(session);
-  
+
+    Set<String> alreadyTested = new HashSet<String>();
     for (DRepresentation representation : DialectManager.INSTANCE.getAllRepresentations(session)) {
       if (representation instanceof DDiagram) {
         DDiagram diagram = (DDiagram) representation;
-        testRequirementsToolsOnDiagram(diagram, sessionContext);
+        String diagramType = diagram.getDescription().getName();
+        if (!alreadyTested.contains(diagramType)) {
+          alreadyTested.add(diagramType);
+          testRequirementsToolsOnDiagram(diagram, sessionContext);
+        }
       }
     }
   }
-  
+
   /**
    * Return the first node target or container target of the diagram
+   * 
    * @param diagram
    * @return
    */
@@ -70,84 +79,86 @@ public class RequirementsToolsTestCase extends AbstractDiagramTestCase {
     }
     return null;
   }
-  
+
   /**
    * Called on each diagram and scenario of the model
+   * 
    * @param rep
    * @param sessionContext
    */
   private void testRequirementsToolsOnDiagram(DDiagram rep, SessionContext sessionContext) {
-      
+    System.out.println("Test requirements tools on diagram: " + rep.getName());
     RequirementsDiagram diagram = new RequirementsDiagram(sessionContext, rep);
     diagram.open();
-    
+
     // test on diagram
-//    testRequirementsToolsOnGivenElement(diagram, diagram.getDiagramId());
-    
+    // testRequirementsToolsOnGivenElement(diagram, diagram.getDiagramId());
+
     // test on a Requirement
     testRequirementsToolsOnGivenElement(diagram, REQ_3, REQ_1, REQ_2);
-    
+
     // test on a Capella Element
     String CAPELLA_ELEMENT_1 = getFirstCapellaElement(diagram);
     if (CAPELLA_ELEMENT_1 == null) {
-      fail("No Capella Element found in this diagram: "+rep.getName());
+      fail("No Capella Element found in this diagram: " + rep.getName());
     } else {
       testRequirementsToolsOnGivenElement(diagram, CAPELLA_ELEMENT_1, REQ_4, REQ_5);
     }
   }
-  
+
   /**
    * called on a capella element, a requirement, and the back of the given diagram
+   * 
    * @param diagram
    * @param id
    * @param IdIsDiagram
    */
-  private void testRequirementsToolsOnGivenElement(RequirementsDiagram diagram, String id, String incomingReq, String outgoingReq) {
-      
+  private void testRequirementsToolsOnGivenElement(RequirementsDiagram diagram, String id, String incomingReq,
+      String outgoingReq) {
+
     // show Requirements
     if (id.equals(REQ_3)) {
       diagram.showRequirements(outgoingReq, incomingReq, REQ_3);
     } else {
       diagram.showRequirements(outgoingReq, incomingReq);
     }
-    
+
     // create Requirement Links
     if (!id.equals(diagram.getDiagramId())) {
       diagram.createRequirementLink(id, outgoingReq);
       diagram.createRequirementLink(incomingReq, id);
     } else {
       // cannot use Requirement Links tool for diagram
-      
+
     }
-    
+
     // hide Incoming/Outgoing Requirements
     diagram.hideOutgoingRequirements(outgoingReq, id);
     diagram.hideIncomingRequirements(incomingReq, id);
-      
+
     // show Incoming/Outgoing Requirements
     diagram.showOutgoingRequirements(outgoingReq, id);
     diagram.showIncomingRequirements(incomingReq, id);
-    
+
     // hide Requirements
-    diagram.hideRequirements(new String[]{outgoingReq, incomingReq});
-    
+    diagram.hideRequirements(new String[] { outgoingReq, incomingReq });
+
     // REQ_3 must stay
     if (id.equals(REQ_3)) {
       diagram.showRequirements(REQ_3);
       diagram.hasView(REQ_3);
     }
-    
+
     // show all Linked Requirements
     diagram.hasntView(outgoingReq);
     diagram.hasntView(incomingReq);
     diagram.showAllLinkedRequirements(id);
     diagram.hasView(outgoingReq);
     diagram.hasView(incomingReq);
-    
+
     // REQ_3 must stay
     if (id.equals(REQ_3)) {
       diagram.hasView(REQ_3);
     }
   }
 }
-
