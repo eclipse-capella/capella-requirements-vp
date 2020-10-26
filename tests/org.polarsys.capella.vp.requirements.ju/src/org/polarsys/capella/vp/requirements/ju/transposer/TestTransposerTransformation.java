77f1143018ed5b58f3804ea55c20fb1e62fe7660
Sandu Postaru <sandu.postaru@thalesgroup.com>
2020-10-26 09:33:17 +0100

    [test] Update Transposer transformation test
    Change-Id: I74b208d586891b97e2d249391382dd1be101a95f
    Signed-off-by: Sandu Postaru <sandu.postaru@thalesgroup.com>

--- tests/org.polarsys.capella.vp.requirements.ju/src/org/polarsys/capella/vp/requirements/ju/transposer/TestTransposerTransformation.java
+++ tests/org.polarsys.capella.vp.requirements.ju/src/org/polarsys/capella/vp/requirements/ju/transposer/TestTransposerTransformation.java
@@ -11,6 +11,7 @@
 package org.polarsys.capella.vp.requirements.ju.transposer;
 
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.polarsys.capella.vp.requirements.importer.transposer.activities.TransposerTransformation;
 import org.polarsys.kitalpha.cadence.core.api.parameter.ActivityParameters;
 
@@ -18,14 +19,15 @@ import org.polarsys.kitalpha.cadence.core.api.parameter.ActivityParameters;
  * @author Joao Barata
  */
 public class TestTransposerTransformation extends TransposerTransformation {
-  
+
   public static String getId() {
     return TestTransposerTransformation.class.getCanonicalName();
   }
-  
+
+  @Override
   protected IStatus _run(ActivityParameters activityParams) {
     // Override this to avoid launching the activity in a Job in testing mode
-    IStatus status = mergeAndSave(activityParams);
+    IStatus status = mergeAndSave(activityParams, new NullProgressMonitor());
     return status;
   }
 }
